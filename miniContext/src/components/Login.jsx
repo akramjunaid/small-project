e4fb37a1e91bcffdbb174b55a7a40536c50a408e
akramjunaid <akramjunaid600@gmail.com>
2024-04-28 17:04:17 +0530

    feat: added useContext api project 1

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import UserContext from "../Context/UserContext";
+import { useState, useContext } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {setUser} = useContext(UserContext);
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    setUser({username, password})
+  };
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="username"
+      />
+      <input
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="password"
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default Login;
